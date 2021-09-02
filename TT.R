991fe7f31c8ace9d9a28eee1be67e926919d2455
SamudraMadushani <samudramadushani@gmail.com>
2021-09-02 02:50:24 -0700

    update

--- TT.R
+++ TT.R
@@ -27,3 +27,27 @@ FBVZW<-pcsW %>%
   labs(x="PC1", y = "PC2")
 
 FBVZW<-FBVZW+ theme(legend.position = "bottom",legend.box = "vertical")
+
+# Change point analysis
+##########################################################################################
+
+par(mar=c(4,4,.3,.3)) 
+set.seed(1)
+# Change in mean example following EFK
+x=1:500
+z=c(rnorm(100,1,sd=0.5),rnorm(150,0,sd=0.5),rnorm(200,2,sd=0.5),rnorm(50,0.5,sd=0.5))
+plot(x,z,type='l',xlab='',ylab='')
+lines(x=1:100,y=rep(1,100),col='red',lwd=3)
+lines(x=101:250,y=rep(0,150),col='red',lwd=3)
+lines(x=251:450,y=rep(2,200),col='red',lwd=3)
+lines(x=451:500,y=rep(0.5,50),col='red',lwd=3)
+# Change in variance example following EFK
+x=1:500
+z=c(rnorm(100,0,sd=0.1),rnorm(150,0,sd=0.7),rnorm(200,0,sd=0.25),rnorm(50,0,sd=1))
+plot(x,z,type='l',xlab='',ylab='')
+
+
+
+
+
+
